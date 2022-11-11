e55b6220cdc3e4e28e0b2762c095024a7ae78f58
gonsanmar2 <gonsanmar2@alum.us.es>
2022-11-11 10:26:05 +0100

    Logro CRUD entero

--- src/main/java/org/springframework/samples/petclinic/logro/LogroController.java
+++ src/main/java/org/springframework/samples/petclinic/logro/LogroController.java
@@ -1,29 +1,97 @@
 package org.springframework.samples.petclinic.logro;
 
-import java.util.List;
+import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/logros")
 public class LogroController {
     private static final String LOGRO_LISTING="logros/LogrosListing";
-    private LogroService logroService;
+    private static final String LOGRO_EDIT="logros/EditLogro";
 
     @Autowired
-    public LogroController(LogroService logroService){
-        this.logroService=logroService;
-    }
-
-    @GetMapping
-    public String listAllLogros(ModelMap model){
-        List<Logro> allLogros = this.logroService.getAllLogros();
-        model.put("logros", allLogros);
-        return LOGRO_LISTING;
-    }
+    LogroService logroService;
     
+     @GetMapping
+     public ModelAndView showLogrosListing() {
+         ModelAndView result=new ModelAndView(LOGRO_LISTING);
+         result.addObject("logros",logroService.getAllLogros());
+         return result;
+     }
+     
+     @GetMapping("/delete/{id}")
+     public ModelAndView deleteRoom(@PathVariable("id") int id) {
+         logroService.removeLogro(id);
+         ModelAndView result=showLogrosListing();
+         result.addObject("message", "Room removed successfully");
+         return result;
+     }
+     
+     
+     @GetMapping("/edit/{id}")
+     public ModelAndView editRoom(@PathVariable("id") int id) {
+         ModelAndView result=new ModelAndView(LOGRO_EDIT);
+         Logro logro=logroService.getLogro(id);
+         if(logro!=null)
+             result.addObject("logro", logro);
+         else
+             result=showLogrosListing();                          
+         return result;
+     }
+     
+     @PostMapping("/edit/{id}")
+     public ModelAndView editRoom(@PathVariable("id") int id, @Valid Logro logro,BindingResult br) {        
+         ModelAndView result=null;
+         if(br.hasErrors()) {
+             result=new ModelAndView(LOGRO_EDIT);
+             result.addAllObjects(br.getModel());         
+         }else {
+             Logro logroToUpdate=logroService.getLogro(id);
+             
+             if(logroToUpdate!=null) {
+                 BeanUtils.copyProperties(logro, logroToUpdate,"id");                 
+                 logroService.save(logroToUpdate);
+                 result=showLogrosListing();
+                 result.addObject("message", "Logro saved succesfully!");
+             }else {
+                 result=showLogrosListing();             
+                 result.addObject("message", "Logro with id "+id+" not found!");
+             }
+         }                                                
+         return result;
+     }
+     
+     @GetMapping("/create")
+     public ModelAndView createRoom() {
+         ModelAndView result=new ModelAndView(LOGRO_EDIT);
+         Logro logro=new Logro();         
+         result.addObject("logro", logro);                                  
+         return result;
+     }
+     
+     
+     @PostMapping("/create")
+     public ModelAndView saveNewRoom(@Valid Logro logro,BindingResult br) {        
+         ModelAndView result=null;
+         if(br.hasErrors()) {
+             result=new ModelAndView(LOGRO_EDIT);
+             result.addAllObjects(br.getModel());         
+         }else {                          
+             logroService.save(logro);
+             result=showLogrosListing();
+             result.addObject("message", "Logro saved succesfully!");             
+         }                                                
+         return result;
+     }
+     
+     
 }
--- src/main/java/org/springframework/samples/petclinic/logro/LogroService.java
+++ src/main/java/org/springframework/samples/petclinic/logro/LogroService.java
@@ -1,22 +1,40 @@
 package org.springframework.samples.petclinic.logro;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 @Service
 public class LogroService {	
 	
 	private LogroRepository logroRepository;
 
 	@Autowired
-	public LogroService(LogroRepository logroRepository){
-		this.logroRepository=logroRepository;
+	public LogroService(LogroRepository logroRepository) {
+		this.logroRepository = logroRepository;
 	}
 
-	public List<Logro> getAllLogros(){
+	@Transactional(readOnly = true)
+	public List<Logro> getAllLogros() {
 		return this.logroRepository.findAll();
+	}
+
+	@Transactional
+	public void removeLogro(long id) {
+		this.logroRepository.deleteById(id);
+	}
+
+	@Transactional(readOnly = true)
+	public Logro getLogro(int id) {
+		Optional<Logro> logro = this.logroRepository.findById((long) id);
+		return logro.isPresent() ? logro.get() : null;
+	}
+
+	@Transactional
+	public Logro save(Logro logro) {
+		return this.logroRepository.save(logro);
 	}
 	
 }
