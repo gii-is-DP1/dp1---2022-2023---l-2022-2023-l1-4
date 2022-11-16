a414a966b70121593b971d0f69d3233a6816aae6
Pablo Pino Mateo <pabpinmat@alum.us.es>
2022-11-16 19:10:36 +0100

    Update Game y ServiceTEST y ControllerTest incompleto

--- src/main/resources/db/hsqldb/data.sql
+++ src/main/resources/db/hsqldb/data.sql
@@ -123,3 +123,5 @@ INSERT INTO visits(id,pet_id,visit_date,description) VALUES (4, 7, '2013-01-04',
 --Me asigno los datos del jugador:
 INSERT INTO players VALUES (1, 'Carlos', 'Nuchera', 'carlosnuchera98@gmail.com', '669081623', 'carnucbol1');
 INSERT INTO players VALUES (2, 'Pablo', 'Pino', 'pablop@gmail.com', '696969696', 'pablop');
+
+INSERT INTO games(id,accessible,name,num_players) VALUES(1,TRUE,'caca',2);
