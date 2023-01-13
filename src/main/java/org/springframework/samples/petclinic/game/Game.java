b53ac82675adecbb564612308936be1ff5fc7f24
ferbarbar1 <ferbarbar1@alum.us.es>
2023-01-13 17:33:59 +0100

    Validación añadida a nombre de partida

--- src/main/java/org/springframework/samples/petclinic/game/Game.java
+++ src/main/java/org/springframework/samples/petclinic/game/Game.java
@@ -10,6 +10,7 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.player.Player;
@@ -25,6 +26,7 @@ public class Game  extends BaseEntity{
 	
 	@Column(unique = true,name = "name")
     @NotBlank
+	@Size(min = 4, max = 15, message="El nombre de la partida tiene que tener entre 4 y 15 caracteres")
 	private String name;
 
 	@NotNull
