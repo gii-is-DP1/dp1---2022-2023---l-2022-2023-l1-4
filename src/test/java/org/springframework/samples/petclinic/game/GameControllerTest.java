841072c9b6782c5315d9b39eb58c2d94f2158827
CarlosNuchera <carnucbol@alum.us.es>
2023-01-13 11:50:53 +0100

    Test hechos menos algunos de game y minijuego controller

--- src/test/java/org/springframework/samples/petclinic/game/GameControllerTest.java
+++ src/test/java/org/springframework/samples/petclinic/game/GameControllerTest.java
@@ -1,4 +1,5 @@
 package org.springframework.samples.petclinic.game;
+
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
@@ -17,6 +18,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
 import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
 import org.springframework.samples.petclinic.user.AuthoritiesService;
 import org.springframework.samples.petclinic.user.UserService;
@@ -33,190 +35,64 @@ import org.springframework.test.web.servlet.MockMvc;
 @WebMvcTest(controllers = GameController.class, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class), excludeAutoConfiguration = SecurityConfiguration.class)
 class GameControllerTests {
 
-	private static final int TEST_GAME_ID = 1
-	;
 
+	private static String VIEWS_GAME_CREATE_OR_UPDATE_FORM = "games/new";
+	private static final int TEST_GAME_ID = 40;
+	private static final int TEST_PLAYER_ID = 6;
 	@Autowired
+	@MockBean
 	private GameController gameController;
 
 	@MockBean
 	private GameService gameService;
+
+	@MockBean
+	private AuthoritiesService authoritiesService;
 
 	@Autowired
 	private MockMvc mockMvc;
 
 	private Game game;
 
-	/*@BeforeEach
-	void setUp(){
-		game = new Game();
-		game.setId(TEST_GAME_ID);
-		game.setName("Prueba");
-		game.setNumPlayers(2);
-
-	}*/
-/* 
 	@BeforeEach
-	void setup() {
-
-		
-		User user1 = new User();
-        user1.setUsername("user1");
-        Player player1 = new Player();
-        player1.setId(20);
-        player1.setFirstName("George");
-        player1.setLastName("Franklin");
-        player1.setTelephone("6085551023");
-        player1.setUser(user1);
-
-        User user2 = new User();
-        user2.setUsername("user2");
-        Player player2 = new Player();
-        player2.setId(30);
-        player2.setFirstName("Abraham");
-        player2.setLastName("Lincon");
-        player2.setTelephone("123456789");
-        player2.setUser(user2);
-
-        List<Player> players = new ArrayList<>();
-        players.add(player1);
-        players.add(player2);
-        
-
-        Minijuego minijuego1 = new Minijuego();
-        Minijuego minijuego2 = new Minijuego();
-        minijuego1.setId(5);
-        minijuego1.setName("Torre Infernal");
-        minijuego2.setId(6);
-        minijuego2.setName("El Foso");
-
+	void setUp() {
 		game = new Game();
+		game.setName("PruebaPost");
+		game.setNumPlayers(2);
 		game.setId(TEST_GAME_ID);
-		game.setName("Prueba");
-		game.setNumPlayers(3);
-		gameService.save(game);
 
 	}
-	*/
-	@WithMockUser(value = "spring")
+
 	@Test
-	void testInitCreationForm() throws Exception {
+	@WithMockUser
+	public void shouldReturnCreateGameViewOnNewGame() throws Exception {
 		mockMvc.perform(get("/games/new"))
-            .andExpect(status().isOk()).andExpect(model().attributeExists("game"))
-            .andExpect(view().name("games/createOrUpdateGameForm"));
-		}
-	/*
+				.andExpect(status().isOk())
+				.andExpect(view().name(VIEWS_GAME_CREATE_OR_UPDATE_FORM));
+	}
 	@WithMockUser(value = "spring")
 	@Test
 	void testProcessCreationFormSuccess() throws Exception {
-		mockMvc.perform(post("/games/new").param("name", "Prueba").param("numPlayers", "2"))
-		.andExpect(view().name("redirect:/games/rondas/new"));
+		mockMvc.perform(post("/games/new").param("name", "PruebaPost").param("numPlayers", "2").with(csrf()))
+		.andExpect(status().isOk());
 	}
-*/
 
+	@WithMockUser(value = "spring")
+	@Test
+	void testProcessInitFindForm() throws Exception {
+		given(this.gameService.getAllGames()).willReturn(Lists.newArrayList(game, new Game()));
 
+		mockMvc.perform(get("/games"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("games"));
+	}
 
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessCreationFormSuccess() throws Exception {
-	// 	mockMvc.perform(post("/games/new").param("name", "partidaPrueba").param("lastName", "Bloggs").with(csrf())
-	// 			.param("email", "joe@gmail.com").param("telephone", "666681623"))
-	// 			.andExpect(status().is3xxRedirection());
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessCreationFormHasErrors() throws Exception {
-	// 	mockMvc.perform(post("/games/new").with(csrf()).param("firstName", "Joe").param("lastName", "Bloggs")
-	// 			.param("email", "emailincorrecto")).andExpect(status().isOk()).andExpect(model().attributeHasErrors("game"))
-	// 			.andExpect(model().attributeHasFieldErrors("game", "telephone"))
-	// 			.andExpect(view().name("games/createOrUpdateGameForm"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testInitFindForm() throws Exception {
-	// 	mockMvc.perform(get("/games/find")).andExpect(status().isOk()).andExpect(model().attributeExists("game"))
-	// 			.andExpect(view().name("games/findGames"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessFindFormSuccess() throws Exception {
-	// 	given(this.gameService.findGameByLastName("")).willReturn(Lists.newArrayList(carlos, new Game()));
-
-	// 	mockMvc.perform(get("/games")).andExpect(status().isOk()).andExpect(view().name("games/gamesList"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessFindFormByLastName() throws Exception {
-	// 	given(this.gameService.findGameByLastName(partidaPrueba.getLastName())).willReturn(Lists.newArrayList(partidaPrueba));
-
-	// 	mockMvc.perform(get("/games").param("lastName", "Nuchera")).andExpect(status().is3xxRedirection())
-	// 			.andExpect(view().name("redirect:/games/" + TEST_GAME_ID));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessFindFormNoGamesFound() throws Exception {
-	// 	mockMvc.perform(get("/games").param("lastName", "Unknown Surname")).andExpect(status().isOk())
-	// 			.andExpect(model().attributeHasFieldErrors("game", "lastName"))
-	// 			.andExpect(model().attributeHasFieldErrorCode("game", "lastName", "notFound"))
-	// 			.andExpect(view().name("games/findGames"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testInitUpdateGameForm() throws Exception {
-	// 	mockMvc.perform(get("/games/{gameId}/edit", TEST_GAME_ID)).andExpect(status().isOk())
-	// 			.andExpect(model().attributeExists("game"))
-	// 			.andExpect(model().attribute("game", hasProperty("lastName", is("Nuchera"))))
-	// 			.andExpect(model().attribute("game", hasProperty("firstName", is("Carlos"))))
-	// 			.andExpect(model().attribute("game", hasProperty("email", is("carlosnuchera98@gmail.com"))))
-	// 			.andExpect(model().attribute("game", hasProperty("telephone", is("669081623"))))
-	// 			.andExpect(view().name("games/createOrUpdateGameForm"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessUpdateGameFormSuccess() throws Exception {
-	// 	mockMvc.perform(post("/games/{gameId}/edit", TEST_GAME_ID).with(csrf()).param("firstName", "Joe")
-	// 			.param("lastName", "Bloggs").param("address", "123 Caramel Street").param("city", "London")
-	// 			.param("telephone", "01616291589")).andExpect(status().is3xxRedirection())
-	// 			.andExpect(view().name("redirect:/games/{gameId}"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testProcessUpdateGameFormHasErrors() throws Exception {
-	// 	mockMvc.perform(post("/games/{gameId}/edit", TEST_GAME_ID).with(csrf()).param("firstName", "Joe")
-	// 			.param("lastName", "Bloggs").param("email", "joe@gmail.com")).andExpect(status().isOk())
-	// 			.andExpect(model().attributeHasErrors("game"))
-	// 			.andExpect(model().attributeHasFieldErrors("game", "telephone"))
-	// 			.andExpect(view().name("games/createOrUpdateGameForm"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testShowPlayer() throws Exception {
-	// 	mockMvc.perform(get("/players/{playerId}", TEST_GAME_ID)).andExpect(status().isOk())
-	// 			.andExpect(model().attribute("player", hasProperty("lastName", is("Nuchera"))))
-	// 			.andExpect(model().attribute("player", hasProperty("firstName", is("Carlos"))))
-	// 			.andExpect(model().attribute("player", hasProperty("email", is("carlosnuchera98@gmail.com"))))
-	// 			.andExpect(model().attribute("player", hasProperty("telephone", is("669081623"))))
-	// 			.andExpect(view().name("players/playerDetails"));
-	// }
-
-	// @WithMockUser(value = "spring")
-	// @Test
-	// void testDeletePlayerById() throws Exception {
-	// 	mockMvc.perform(get("/players/delete/{playerId}", TEST_PLAYER_ID)).andExpect(status().isOk())
-	// 			.andExpect(model().attribute("player", hasProperty("lastName", is("Nuchera"))))
-	// 			.andExpect(model().attribute("player", hasProperty("firstName", is("Carlos"))))
-	// 			.andExpect(model().attribute("player", hasProperty("email", is("carlosnuchera98@gmail.com"))))
-	// 			.andExpect(model().attribute("player", hasProperty("telephone", is("669081623"))))
-	// 			.andExpect(view().name("players"));
-	// }
+	@WithMockUser(value = "spring")
+	@Test
+	void testJoinGame() throws Exception {
+		mockMvc.perform(get("/games"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("games"));
+	}
 
 }
